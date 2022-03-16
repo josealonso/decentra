2dfd4604e81f0c64a51a16bf9a1b86bc440ca12b
sky cherfa <sherifcherfa@gmail.com>
2022-03-15 23:52:51 -0400

    added oauth and connect user account to a profile

--- components/layout/Navbar/index.js
+++ components/layout/Navbar/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import { auth } from '../../../lib/firebase';
+import { useContext } from 'react';
+import { UserContext } from '../../../lib/context';
 import Link from 'next/link'
 
 import styles from './styles.module.css'
 
 export default function Navbar() {
 
-  const user = null;
-  const username = null;
+  const {user, username} = useContext(UserContext)
 
   return (
     <nav className={styles.navbar}>
@@ -19,17 +20,23 @@ export default function Navbar() {
         </li>
 
         {
-          username && (
+          user && (
             <>
               <li className={styles.pushLeft}>
                 <Link href="/admin">
                   <button className={styles.btnBlue}>Write Posts</button>
                 </Link>
               </li>
+
+              <li className={styles.pushLeft}>
+                <Link href="/admin">
+                  <button className={styles.btnBlue} onClick={() => auth.signOut()}>Log Out</button>
+                </Link>
+              </li>
              
              <li>
                 <Link href={`/${username}`}>
-                  <img src={user?.photoURL}></img>
+                  <img style={{height: '5vh'}} src={user?.photoURL}></img>
                 </Link>
              </li>
             </>
@@ -37,7 +44,7 @@ export default function Navbar() {
         }
 
         {
-          !username && (
+          !user && (
             <>
               <li>
                 <Link href={`/Enter`}>
