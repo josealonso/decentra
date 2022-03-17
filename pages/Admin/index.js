32b2b88db3c011b989d6b33a912bd34154b90367
sky cherfa <sherifcherfa@gmail.com>
2022-03-17 18:01:20 -0400

    left off with bug in username and admin

---
+++ pages/Admin/index.js
@@ -0,0 +1,91 @@
+import AuthCheck from "../../components/helpers/AuthCheck";
+import { UserContext } from "../../lib/context";
+import { useRouter } from 'next/router';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import PostFeed from '../../components/layout/PostFeed'
+import { firestore, auth, serverTimestamp } from '../../lib/firebase';
+import kebabCase from 'lodash.kebabcase';
+import toast from 'react-hot-toast';
+
+import { useContext, useState } from 'react';
+import styles from './styles.module.css'
+
+export default function AdminPostsPage({}) {
+  return (
+    <main className={styles.container}>
+      <AuthCheck>
+        <PostList />
+        <CreateNewPost />
+      </AuthCheck>
+    </main>
+  )
+}
+
+function PostList() {
+  const ref = firestore.collection('users').doc(auth.currentUser.uid).collection('posts');
+  
+  const query = ref.orderBy('createdAt');
+  const [querySnapshot] = useCollection(query);
+
+  const posts = querySnapshot?.docs.map((doc) => doc.data());
+
+  return (
+    <>
+      <h1>Manager your Posts!</h1>
+      <PostFeed posts={posts} admin/>
+    </>
+  )
+}
+
+function CreateNewPost() {
+  const router = useRouter();
+  const { username } = useContext(UserContext);
+  const [title, setTitle] = useState('');
+
+  const slug = encodeURI(kebabCase(title));
+
+  const isValid = title.length > 3 && title.length < 100;
+
+  const createPost = async (e) => {
+    e.preventDefault();
+
+    const uid = auth.currentUser.uid;
+    const ref = firestore.collection('users').doc(uid).collection('posts').doc(slug);
+
+    const data = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: '# hello world!',
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      heartCount: 0,
+    }
+
+    await ref.set(data);
+    toast.success('Post created!')
+    router.push(`/Admin/${slug}`);
+  } 
+
+  return (
+    <form onSubmit={createPost}>
+      <input 
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="My Awesome Post"
+        className={styles.input}
+      />
+      <p>
+        <strong>
+          Slug: 
+        </strong>
+        {slug}
+      </p>
+      <button type="submit" disabled={!isValid} className={styles.submit}>
+        Create New Post
+      </button>
+    </form>
+  )
+}
--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,7 +1,67 @@
 import React from 'react'
+import PostContent from '../../components/layout/PostContent/index.js'
+import {getUserWithUsername, firestore, postToJSON } from '../../lib/firebase'
+import { useDocumentData } from 'react-firebase-hooks/firestore'
+import styles from './styles.module.css'
 
-export default function postPage() {
+
+export async function getStaticProps({ params }) {
+  const {username,slug} = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if(userDoc){
+    const postRef = userDoc.ref.collection('posts').doc(slug);
+    post = postToJSON(await postRef.get());
+  
+    path = postRef.path;
+  }
+
+  return {
+    props: {post, path},
+    revalidate: 5000,
+  }
+}
+
+export async function getStaticPaths() {
+
+  const snapshot = await firestore.collectionGroup('posts').get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+
+    return {
+      params: {username, slug},
+    };
+  })
+
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export default function Post(props) {
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
+
+  const post = realtimePost || props.post;
+
   return (
-    <div>postPage</div>
-  )
+    <main className={styles.container}>
+
+      <section>
+        <PostContent post={post} />
+      </section>
+
+      <aside className="card">
+        <p>
+          <strong>{post.heartCount || 0} 🤍</strong>
+        </p>
+
+      </aside>
+    </main>
+  );
 }
