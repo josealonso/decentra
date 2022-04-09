9f47350dad942606539202d60b9abd73c1c86fc4
sky cherfa <sherifcherfa@gmail.com>
2022-04-08 22:42:05 -0400

    moved LinkItem Component

---
+++ components/simple/LinkItem/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+export default function LinkItem({link}) {
+  return (
+    <a className={styles.link_block} href={link.link} target="_blank">
+      <img src={link.icon} className={styles.white_icon}/>
+      <h2>{link.title}</h2>
+    </a>
+  )
+}
