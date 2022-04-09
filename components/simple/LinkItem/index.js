41e1e10c42e1752486221bdd383f7fcf15396483
sky cherfa <sherifcherfa@gmail.com>
2022-04-09 13:01:30 -0400

    changed image to upload instead of link

--- components/simple/LinkItem/index.js
+++ components/simple/LinkItem/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.scss'
 export default function LinkItem({link}) {
   return (
     <a className={styles.link_block} href={link.link} target="_blank">
-      <div className={styles.icon}> <ReactMarkdown>{link.icon}</ReactMarkdown>  </div>
+      <div className={styles.icon}> <ReactMarkdown>{link?.icon}</ReactMarkdown>  </div>
       <h2>{link.title}</h2>
     </a>
   )
