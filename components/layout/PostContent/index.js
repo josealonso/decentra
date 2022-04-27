85482e7386a7da68d6f0917cf44b96f1ea12dffa
sky cherfa <sherifcherfa@gmail.com>
2022-04-27 10:50:27 -0400

    fixed broken section

--- components/layout/PostContent/index.js
+++ components/layout/PostContent/index.js
@@ -7,16 +7,8 @@ export default function PostContent({ post }) {
 
   return (
     <div className={styles.card}>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <ReactMarkdown>{post?.icon}</ReactMarkdown>
-=======
-      <div className={styles.kicker}
-      <a>Home</a> / <a>Blog</a> / <a>{post?.title}</a>
-      </div>
->>>>>>> d5262f9c32c88df934ad8d3868808d911ad94d1e
-=======
->>>>>>> e232e7cb3f65ba6684475edd58c6274205136435
+     
       <h1 className={styles.title}>{post?.title}</h1>
       <span className={styles.small_title}>
         Written by{' '}
