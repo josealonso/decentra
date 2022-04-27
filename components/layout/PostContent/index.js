1a4e0ebf0f1950cc332a80197aa4155303556b11
D <95552251+d3centra@users.noreply.github.com>
2022-04-27 17:26:11 +0200

    Change post content structure

--- components/layout/PostContent/index.js
+++ components/layout/PostContent/index.js
@@ -6,19 +6,18 @@ export default function PostContent({ post }) {
   const createdAt = typeof post?.createdAt === 'number' ? new Date(post.createdAt) : post.createdAt.toDate();
 
   return (
-    <div className={styles.card}>
-      <ReactMarkdown>{post?.icon}</ReactMarkdown>
-     
+    <div className={styles.card}>   
       <h1 className={styles.title}>{post?.title}</h1>
       <h3>{post?.subtitle}</h3>
+      <ReactMarkdown>{post?.icon}</ReactMarkdown>
       <span className={styles.small_title}>
         Written by{' '}
         <Link href={`/${post.username}/`}>
-          <a  className={styles.small_title}>{post.username}</a>
+          <a className={styles.small_title}>{post.username}</a>
         </Link>{' '}
         on {createdAt.toISOString()}
+        </span>
         <ReactMarkdown>{post?.content}</ReactMarkdown>
-      </span>
     </div>
   );
 }
