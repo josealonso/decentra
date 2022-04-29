1cef53c20a919312f455864addfbc7c8d02ec44c
sky cherfa <sherifcherfa@gmail.com>
2022-04-29 15:52:59 -0400

    added a conditional render button

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,29 @@ import '../theme/global.scss';
 import { Toaster } from 'react-hot-toast';
 import { WalletEthers } from '@components/helpers/ConnectWallet';
 import { UserContext } from '../lib/context';
+import { useState } from 'react';
 import { useUserData } from '../lib/hooks';
-
+import styles from './styles.module.scss'
 
 function MyApp({ Component, pageProps }) {
   const userData = useUserData();
+
+  const [web3Active, setWeb3] = useState(false)
 
   return (
     <>
       <UserContext.Provider value={userData}>
       <Navbar/>
-      <WalletEthers />
+
+      <button className={styles.special_button} onClick={() => {setWeb3(true)}}>Do web3 stuff</button>
+
+      {
+        web3Active ? 
+        <WalletEthers />
+        :
+        ''
+      }
+  
       <PageWrapper>
         <Component {...pageProps} />
       </PageWrapper>
