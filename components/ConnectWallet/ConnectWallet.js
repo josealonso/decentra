a6e6fd115bb5d04cc03239de3412ae6d863ceb3d
josealonso <jose.alonso.programmer@gmail.com>
2022-05-09 12:33:13 +0200

    Deleted two components

--- components/ConnectWallet/ConnectWallet.js
+++
@@ -1,6 +0,0 @@
-// This is not going to be a traditional component, in that you are going to use it both as a component and as a piece of data for a special Hook called useContext. For now, keep the flat file structure, but if you use a lot of contexts, it might be worth moving them to a different directory structure.
-import { createContext } from 'react';
-
-const WalletContext = createContext();
-export default WalletContext; // The context is registered
-
