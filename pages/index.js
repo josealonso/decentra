e552b8f2692b647b5ba47489c29a3f6ef8b27290
josealonso <jose.alonso.programmer@gmail.com>
2022-05-09 13:29:04 +0200

    Used the hook for the "Connecting Metamask" button

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
+import React, { useState } from 'react'
 import PostFeed from '../components/layout/PostFeed';
 import Loader from '../components/simple/Loader';
 import { firestore, fromMillis, postToJSON } from '../lib/firebase';
@@ -7,7 +8,7 @@ import styles from './styles.module.scss'
 
 const LIMIT = 5;
 
-export async function getServerSideProps(context){
+export async function getServerSideProps(context) {
   const postsQuery = firestore
     .collectionGroup('posts')
     .where('published', '==', true)
@@ -17,11 +18,14 @@ export async function getServerSideProps(context){
   const posts = (await postsQuery.get()).docs.map(postToJSON)
 
   return {
-    props: {posts},
+    props: { posts },
   }
 }
 
 export default function Home(props) {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  const disconnectWallet = useDisconnect();
 
   const [posts, setPosts] = useState(props.posts);
   const [loading, setLoading] = useState(false);
@@ -34,20 +38,20 @@ export default function Home(props) {
     const last = posts[posts.length - 1];
 
     const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
-  
+
     const query = firestore
       .collectionGroup('posts')
       .where('published', '==', true)
       .orderBy('createdAt', 'desc')
       .startAfter(cursor)
       .limit(LIMIT);
-    
+
     const newPosts = (await query.get()).docs.map((doc) => doc.data());
 
     setPosts(posts.concat(newPosts));
     setLoading(false)
 
-    if(newPosts.length < LIMIT) {
+    if (newPosts.length < LIMIT) {
       setPostsEnd(true);
     }
   }
@@ -56,14 +60,14 @@ export default function Home(props) {
 
   return (
     <main className={styles.main}>
-  
+
 
       <div className="card card-info">
         <h2>Welcome to Decentra</h2>
         <p>A better way to organise your projects.</p>
         <p>Decentra is the future of social collaboration tools, providing users everything they need to create fully functional projects</p>
       </div>
-     
+
       <PostFeed posts={posts} />
 
       {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
@@ -71,6 +75,19 @@ export default function Home(props) {
       <Loader show={loading} />
 
       {postsEnd && 'You have reached the end!'}
+
+      <div>
+        {address ? (
+          <>
+            <button onClick={disconnectWallet}>Disconnect Wallet</button>
+            <p>Your address: {address}</p>
+          </>
+        ) : (
+          // <button onClick={connectWithMetamask}>Connect with Metamask</button>
+          <button className={styles.special_button} onClick={connectWithMetamask}><img src="https://i.ibb.co/FX9hrjH/3845819.png" alt="connect-web3-wallet" /></button>
+        )}
+      </div>
+
     </main>
   );
 }
